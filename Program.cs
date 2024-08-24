d08f86bad8403813cc65df289996bea186c36a6a
Bhabishworgrg <bhabishworgrg@gmail.com>
2024-08-24 17:58:02 +0545

    Code main program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Text.RegularExpressions;
+
+static bool MatchPattern(string line, string pattern)
+{
+	return Regex.IsMatch(line, pattern);
+}
+
+string pattern = args[0];
+
+if (!Console.IsInputRedirected || Console.In.Peek() == -1)
+{
+	Console.WriteLine("Input expected: Input values to match from via a pipe.");
+	Environment.Exit(1);
+}
+
+string[] input = Console.In.ReadToEnd().Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+foreach (string line in input)
+{
+	if (MatchPattern(line, pattern))
+	{
+		Console.WriteLine(line);
+	}
+}
+Environment.Exit(0);
